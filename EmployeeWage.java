e82113a6eca0e0dbb21523dc2117d83afc71e7d9
GopalDas <gopalkrdas1997@gmail.com>
2022-05-21 21:45:01 +0530

    Adding part time employee and wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -9,7 +9,7 @@ public class EmployeeWage {
 
        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
        Random random = new Random();
-       int attendance = random.nextInt(2);
+       int attendance = random.nextInt(3);
        int IS_ABSENT_TODAY = 0;
        int IS_FULL_DAY = 1;
        int IS_HALF_DAY = 2;
@@ -18,19 +18,25 @@ public class EmployeeWage {
        int WAGE_PER_HOUR = 20;
        int total_emp_times = 0;
        int total_emp_wage = 0;
+       int HALF_DAY_TIME = 4;
 
        if (attendance == IS_ABSENT_TODAY){
            System.out.println("Today Employee is absent");
-           total_emp_times += ABSENT_DAY_TIME;
-           total_emp_wage += WAGE_PER_HOUR * ABSENT_DAY_TIME;
-
+           total_emp_wage += 0;
 
        }
+       else if (attendance == IS_HALF_DAY)
+       {
+           System.out.println("Employee is working half time today");
+           total_emp_times += HALF_DAY_TIME;
+           total_emp_wage += WAGE_PER_HOUR * HALF_DAY_TIME;
+       }
+
        else{
            System.out.println("Today employee is present");
            total_emp_times += FULL_DAY_TIME;
-           total_emp_wage += WAGE_PER_HOUR * ABSENT_DAY_TIME;
+           total_emp_wage += WAGE_PER_HOUR*FULL_DAY_TIME;
        }
-       System.out.println("Total wage of employee "+total_emp_wage);
+       System.out.println("Total wage of employee= "+total_emp_wage);
    }
 }
