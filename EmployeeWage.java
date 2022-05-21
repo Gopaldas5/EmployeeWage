07ca9876e245d4419173c0261156db64895eb6c2
GopalDas <gopalkrdas1997@gmail.com>
2022-05-21 20:51:19 +0530

    Check Employee is

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,13 +1,26 @@
 package com.bridgeLabz.Day3_EmployeeWageProblem;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.Random;
 
-;
+
 public class EmployeeWage {
    public static void main(String args[]){
 
        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
        Random random = new Random();
+       int attendance = random.nextInt(2);
+       int IS_ABSENT_TODAY = 0;
+       int IS_FULL_DAY = 1;
+       int IS_HALF_DAY = 2;
+
+       if (attendance == IS_ABSENT_TODAY){
+           System.out.println("Today Employee is absent");
+       }
+       else{
+           System.out.println("Today employee is present");
+       }
 
    }
 }
