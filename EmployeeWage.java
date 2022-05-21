3c5b87eb61e33a6333c0e78f0a122108baa6c7a2
GopalDas <gopalkrdas1997@gmail.com>
2022-05-21 20:26:41 +0530

    Displaying Welcome to employee wage computation program

---
+++ EmployeeWage.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz.Day3_EmployeeWageProblem;
+
+import java.util.Random;
+
+;
+public class EmployeeWage {
+   public static void main(String args[]){
+
+       System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+       Random random = new Random();
+
+   }
+}
