6d30389721c2d55e0298f1484c71b8a225fade8c
GopalDas <gopalkrdas1997@gmail.com>
2022-05-21 22:14:02 +0530

    Adding Solving using switch case statement

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -10,9 +10,9 @@ public class EmployeeWage {
        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
        Random random = new Random();
        int attendance = random.nextInt(3);
-       int IS_ABSENT_TODAY = 0;
-       int IS_FULL_DAY = 1;
-       int IS_HALF_DAY = 2;
+       final int IS_ABSENT_TODAY = 0;
+       final int IS_FULL_DAY = 1;
+       final int IS_HALF_DAY = 2;
        int FULL_DAY_TIME = 8;
        int ABSENT_DAY_TIME = 0;
        int WAGE_PER_HOUR = 20;
@@ -20,23 +20,13 @@ public class EmployeeWage {
        int total_emp_wage = 0;
        int HALF_DAY_TIME = 4;
 
-       if (attendance == IS_ABSENT_TODAY){
-           System.out.println("Today Employee is absent");
-           total_emp_wage += 0;
-
-       }
-       else if (attendance == IS_HALF_DAY)
-       {
-           System.out.println("Employee is working half time today");
-           total_emp_times += HALF_DAY_TIME;
-           total_emp_wage += WAGE_PER_HOUR * HALF_DAY_TIME;
-       }
-
-       else{
-           System.out.println("Today employee is present");
-           total_emp_times += FULL_DAY_TIME;
-           total_emp_wage += WAGE_PER_HOUR*FULL_DAY_TIME;
-       }
+       total_emp_times = switch (attendance) {
+           case IS_FULL_DAY -> FULL_DAY_TIME;
+           case IS_HALF_DAY -> HALF_DAY_TIME;
+           case IS_ABSENT_TODAY -> ABSENT_DAY_TIME;
+           default -> 0;
+       };
+       total_emp_wage += WAGE_PER_HOUR*total_emp_times;
        System.out.println("Total wage of employee= "+total_emp_wage);
    }
 }
